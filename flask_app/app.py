c4505259787e1d5adebfecef25e513f8d3e216d6
Cody <73506123+LaplaceCherub@users.noreply.github.com>
2022-11-07 17:49:59 -0700

    Adding LDA model to form

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,6 +2,12 @@ from flask import Flask, redirect, url_for, render_template, request
 import spacy
 import pickle
 import pandas as pd
+import gensim
+import nltk
+from nltk.corpus import stopwords 
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+import string
 
 app = Flask(__name__)
 
@@ -45,8 +51,43 @@ def try_out():
         for pred, word in zip(preds, df_list):
             if pred == 1:
                 rtn_ners.append(word[0])
-        rtn_statement = "The entities from the input are:"
-        return render_template('try.html', opener=rtn_statement, text=rtn_ners)
+        ner_opener = "The entities from the input are:"
+
+
+        dct = pickle.load(open('dct.pkl', 'rb')) 
+        lda_model = pickle.load(open('lda.pkl', 'rb'))   
+
+        def _lemmatize_words(sentence):
+            wordnet_map = {'N':wordnet.NOUN, 'V':wordnet.VERB, 'J':wordnet.ADJ, 'R':wordnet.ADV}
+            pos_tagged_text = nltk.pos_tag(sentence.split())
+            return ' '.join([WordNetLemmatizer().lemmatize(word, wordnet_map.get(pos[0], wordnet.NOUN)) for word, pos in pos_tagged_text])
+    
+        def lda_sent_process(text):
+            text = text.lower()  
+            PUNCT_TO_REMOVE = string.punctuation
+            text = text.translate(str.maketrans('', '', PUNCT_TO_REMOVE))
+            STOPWORDS = set(stopwords.words('english'))
+            text = ' '.join([word for word in text.split() if word not in STOPWORDS])
+            text = _lemmatize_words(text)
+            return text.split()
+
+        def get_topics(new_text, lda_model, dct): 
+            '''
+            new_text: str
+            lda_model: load from lda.pkl
+            dct: load from dct.pkl
+            '''
+            rtn_list = []
+            new_text_doc = lda_sent_process(new_text)
+            topics = lda_model[dct.doc2bow(new_text_doc)]
+            for topic in topics: 
+                rtn_list.append(f'Topic {topic[0]} with probability {topic[1]}')
+            return rtn_list
+
+        lda_opener = 'The clusters from the input are:'
+        lda_text = get_topics(text, lda_model, dct)
+    
+        return render_template('try.html', ner_opener=ner_opener, ner_text=rtn_ners, lda_opener=lda_opener, lda_text=lda_text)
     return render_template('try.html')
 
 if __name__ == '__main__':
